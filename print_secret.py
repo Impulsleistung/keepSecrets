e2448f7224ccc7dad130511f5641c8d605d44b30
impulsleistung <info@impulsleistung.de>
2023-08-12 09:22:14 +0200

    update

--- print_secret.py
+++ print_secret.py
@@ -2,11 +2,7 @@ import pandas as pd
 import os
 
 # Set the display options
-pd.set_option('display.max_rows', 10)
-pd.set_option('display.max_columns', 10)
-
 file_path = os.path.join(os.environ['HOME'], 'decrypted_file.json')
 df = pd.read_json(file_path)
 
-df_str = df.to_string()
-print(df_str)
+print(df.iloc[:, 0])
