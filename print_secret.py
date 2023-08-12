f0ec11ad09ce208b41b2630ee41d1aaf00300811
impulsleistung <info@impulsleistung.de>
2023-08-12 12:50:35 +0200

    push

--- print_secret.py
+++ print_secret.py
@@ -5,4 +5,11 @@ import os
 file_path = os.path.join(os.environ['HOME'], 'decrypted_file.json')
 df = pd.read_json(file_path)
 
-print(df.iloc[:, 0])
+# Create a function to load and display the content of a JSON file using pandas
+def display_json_content(file_path):
+    df = pd.read_json(file_path)
+    return df
+
+# Test the function
+displayed_content = display_json_content(file_path)
+print(displayed_content)
