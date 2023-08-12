c5792e773f8fb650781457725e65fab8e200d5b0
impulsleistung <info@impulsleistung.de>
2023-08-12 09:16:01 +0200

    update

--- print_secret.py
+++ print_secret.py
@@ -8,3 +8,5 @@ pd.set_option('display.max_columns', 10)
 file_path = os.path.join(os.environ['HOME'], 'decrypted_file.json')
 df = pd.read_json(file_path)
 
+df_str = df.to_string(index=False, border=True)
+print(df_str)
