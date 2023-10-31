31b89cde84777d944a0470ed9c64483aa0ab9ca4
Kevin Ostheimer <kevin.ostheimer@cariad.technology>
2023-10-31 09:47:45 +0100

    update 2

---
+++ howto_encrypt/keygen.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+# Generate a string of 100 random characters
+characters = string.ascii_letters + string.digits + string.punctuation
+random_string = ''.join(random.choice(characters) for i in range(100))
+
+# Write the string to a file
+with open('pass.txt', 'w') as file:
+    file.write(random_string)
--- print_secret.py
+++ print_secret.py
@@ -1,16 +1,22 @@
-
 import pandas as pd
 import os
 
-# Set the display options
-file_path = os.path.join(os.environ['HOME'], 'decrypted_file.json')
-df = pd.read_json(file_path)
-
-# Create a function to load and display the content of a JSON file using pandas
 def display_json_content(file_path):
+    """
+    Load and display the content of a JSON file.
+    
+    Args:
+    file_path (str): The path to the JSON file.
+    
+    Returns:
+    DataFrame: A pandas DataFrame containing the contents of the JSON file.
+    """
     df = pd.read_json(file_path)
     return df
 
-# Test the function
-displayed_content = display_json_content(file_path)
+# Set the path to the JSON file
+json_file_path = os.path.join(os.environ['HOME'], 'decrypted_file.json')
+
+# Display the content of the JSON file
+displayed_content = display_json_content(json_file_path)
 print(displayed_content)
